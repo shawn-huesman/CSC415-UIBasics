c4b426ab5bcb2c9a71ce9c6143452108b3f0f325
shawn-huesman <mrshawnhuesman@gmail.com>
2021-09-18 21:15:28 -0400

    Added Android Fundamentals 01.2 Part A
    - Toast button to show short toast
    - Count button that interacts with on-screen counter

--- app/src/main/java/com/example/csc415_uibasics/MainActivity.kt
+++ app/src/main/java/com/example/csc415_uibasics/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.csc415_uibasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var mCount: Int = 0
+    private var mShowCount: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mShowCount = findViewById(R.id.show_count)
+    }
+
+    fun showToast(view: View) {
+        val toast: Toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT)
+
+        toast.show()
+    }
+
+    fun countUp(view: View) {
+        ++mCount
+
+        mShowCount?.setText(mCount.toString())
     }
 }

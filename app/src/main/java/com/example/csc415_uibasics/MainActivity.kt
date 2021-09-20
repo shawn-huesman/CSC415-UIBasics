61fcdcf1947ff9146947b827d94509556815a752
shawn-huesman <mrshawnhuesman@gmail.com>
2021-09-19 21:12:27 -0400

    Completed Android Fundamentals 01.2 Part B Homework
    - Removed RelativeLayout
    - Added ConstraintLayout
    - Created vertical and horizontal layout variants

--- app/src/main/java/com/example/csc415_uibasics/MainActivity.kt
+++ app/src/main/java/com/example/csc415_uibasics/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.example.csc415_uibasics
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
     private var mCount: Int = 0
     private var mShowCount: TextView? = null
+    private var zeroButton: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mShowCount = findViewById(R.id.show_count)
+        zeroButton = findViewById(R.id.button_zero)
     }
 
     fun showToast(view: View) {
@@ -23,8 +26,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun countUp(view: View) {
-        ++mCount
+        val green = ResourcesCompat.getColor(resources, R.color.green, null)
+        val blue = ResourcesCompat.getColor(resources, R.color.blue, null)
+        val primaryColor = ResourcesCompat.getColor(resources, R.color.colorPrimary, null)
 
+        ++mCount
         mShowCount?.setText(mCount.toString())
+
+        zeroButton?.setBackgroundColor(primaryColor)
+
+        if (mCount % 2 == 0) {
+            view.setBackgroundColor(blue)
+        }
+        else {
+            view.setBackgroundColor(green)
+        }
+    }
+
+    fun zeroOutCount(view: View) {
+        val grey = ResourcesCompat.getColor(resources, R.color.grey, null)
+
+        mShowCount?.setText("0")
+        mCount = 0
+
+        view.setBackgroundColor(grey)
     }
 }
